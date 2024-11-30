7dc33cdc8ba7e5cd0b328bf1e4057053caabbccb
Lucas Néia <lucasneia07@gmail.com>
2024-11-30 18:09:40 -0300

    recebendo arquivos infinitamente tcp

--- volumes/maquina1/servidor-tcp.py
+++ volumes/maquina1/servidor-tcp.py
@@ -2,7 +2,7 @@ import socket
 import time
 import os 
 
-def start_tcp_server(host='0.0.0.0', port=5000):
+def start_tcp_server(host='0.0.0.0', port=5001):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
     server_socket.listen(5)
@@ -11,39 +11,59 @@ def start_tcp_server(host='0.0.0.0', port=5000):
     while True:
         client_socket, client_address = server_socket.accept()
         print(f"[INFO] Conexão recebida de {client_address}")
-        
-        # Receber nome do arquivo
-        file_name = client_socket.recv(1024).decode()
-        print(f"[INFO] Recebendo arquivo: {file_name}")
-        
-        # Responder que está pronto para receber
-        client_socket.send("READY".encode())
-        print(f"[INFO] Servidor pronto para receber!")
 
-        pacotes_recebidos = 0
-        bytes_recebidos = 0
+        while True:
+            # Receber nome do arquivo ou comando
+            data = client_socket.recv(1024).decode()
 
-        # Receber o conteúdo do arquivo e salvá-lo
-        with open(file_name, "wb") as file:
-            print("[INFO] Recebendo arquivo...")    
+            if data.lower() == "exit":
+                print("[INFO] Comando 'exit' recebido. Encerrando conexão com o cliente.")
+                client_socket.close()
+                break
             
-            while chunk := client_socket.recv(1024):
-                file.write(chunk)
-                pacotes_recebidos += 1
-                bytes_recebidos += len(chunk)
-                print(f"[RECEBIDO] Chunk {pacotes_recebidos}: {len(chunk)} bytes | Total recebido: {bytes_recebidos} bytes")         
-        
-        # Envia end_time para o cliente
-        end_time = time.time()
-        client_socket.send(str(end_time).encode())
-        file_size = os.path.getsize(file_name)
-        
-        client_socket.close()
-        print(f"[INFO] conexao encerrada")
+            if not data:
+                continue
+            
+            if not data.strip():  # Ignora entradas vazias
+                print("[INFO] Dados inválidos ou vazios recebidos.")
+                continue
 
-        print(f"[ESTATISTICA] Arquivo {file_name} de tamanho {file_size} recebido e salvo com sucesso.")
-        print(f"[ESTATISTICA] Pacotes recebidos: {pacotes_recebidos}")
-        
-        return
+            print(f"[INFO] Recebendo arquivo: {data}")
+            file_name = data
+
+            # Responder que está pronto para receber
+            client_socket.send("READY".encode())
+            print(f"[INFO] Servidor pronto para receber!")
+
+            pacotes_recebidos = 0
+            bytes_recebidos = 0
+
+            # Receber o conteúdo do arquivo e salvá-lo
+            with open(file_name, "wb") as file:
+                print("[INFO] Recebendo arquivo...")    
+
+                while chunk := client_socket.recv(1024):
+                    if chunk.endswith(b"END"):
+                        # Remover o "END" do chunk
+                        chunk = chunk[:-3]
+                        if chunk:  # Escreve os dados restantes, se houver
+                            file.write(chunk)
+                        print("[INFO] Final da transmissão recebido.")
+                        break
+                    file.write(chunk)
+                    pacotes_recebidos += 1
+                    bytes_recebidos += len(chunk)
+                    print(f"[RECEBIDO] Chunk {pacotes_recebidos}: {len(chunk)} bytes | Total recebido: {bytes_recebidos} bytes")         
+
+            # Envia end_time para o cliente
+            end_time = time.time()
+            client_socket.send(str(end_time).encode())
+            file_size = os.path.getsize(file_name)
+
+            print(f"[ESTATISTICA] Arquivo {file_name} de tamanho {file_size} recebido e salvo com sucesso.")
+            print(f"[ESTATISTICA] Pacotes recebidos: {pacotes_recebidos}")
+
+        print(f"[INFO] Conexão encerrada com {client_address}")
+
 if __name__ == "__main__":
     start_tcp_server()
--- volumes/maquina2/cliente-tcp.py
+++ volumes/maquina2/cliente-tcp.py
@@ -5,52 +5,60 @@ import time
 def start_tcp_client(server_host, server_port):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((server_host, server_port))
-    
-    file_path = input("Digite o caminho do arquivo para enviar ao servidor: ")
-    if not os.path.exists(file_path):
-        print(f"[ERRO] Arquivo {file_path} não foi encontrado!")
-        client_socket.close()
-        return
-    
-    # Enviar nome do arquivo
-    file_name = os.path.basename(file_path)
-    client_socket.send(file_name.encode())
-    file_size = os.path.getsize(file_path) # esse tamanho sera usado para comparacao
-    print(f"[INFO] Iniciando envio do arquivo: {file_path}")
-    print(f"[INFO] Tamanho total do arquivo: {file_size} bytes")
-    
-    # Confirmar prontidão do servidor
-    server_ready = client_socket.recv(1024).decode()
-    if server_ready != "READY":
-        print("[ERRO] o servidor não está pronto para receber o arquivo.")
-        client_socket.close()
-        return
-    
-    print("[INFO] o servidor está pronto para receber o arquivo.")
 
-    pacotes_enviados = 0
-    bytes_enviados = 0
-    start_time = time.time()
-    # Enviar o conteúdo do arquivo
-    with open(file_path, "rb") as file:
-        while chunk := file.read(1024):
-            client_socket.send(chunk)
-            pacotes_enviados += 1
-            bytes_enviados += len(chunk)
-            print(f"[ENVIADO] pacote {pacotes_enviados}: {len(chunk)} bytes | Total enviado: {bytes_enviados} bytes")
-            
-    # Fechar a transmissão de dados (importante!)
-    client_socket.shutdown(socket.SHUT_WR)
-    print(f"[INFO] conexao encerrada!")
-    # Recebe o end_time do servidor
-    server_time = client_socket.recv(1024).decode()
+    while True:
+        file_path = input("Digite o caminho do arquivo para enviar ao servidor (ou 'exit' para encerrar): ")
+        
+        if file_path.lower() == 'exit':
+            client_socket.send("exit".encode())
+            print("[INFO] Enviando comando de encerramento ao servidor.")
+            break
+
+        if not os.path.exists(file_path):
+            print(f"[ERRO] Arquivo {file_path} não foi encontrado!")
+            continue
+
+        # Enviar nome do arquivo
+        file_name = os.path.basename(file_path)
+        client_socket.send(file_name.encode())
+        file_size = os.path.getsize(file_path) # esse tamanho será usado para comparação
+        print(f"[INFO] Iniciando envio do arquivo: {file_path}")
+        print(f"[INFO] Tamanho total do arquivo: {file_size} bytes")
+
+        # Confirmar prontidão do servidor
+        server_ready = client_socket.recv(1024).decode()
+        if server_ready != "READY":
+            print("[ERRO] O servidor não está pronto para receber o arquivo.")
+            continue
+
+        print("[INFO] O servidor está pronto para receber o arquivo.")
+
+        pacotes_enviados = 0
+        bytes_enviados = 0
+        start_time = time.time()
+
+        # Enviar o conteúdo do arquivo
+        with open(file_path, "rb") as file:
+            while chunk := file.read(1024):
+                client_socket.send(chunk)
+                pacotes_enviados += 1
+                bytes_enviados += len(chunk)
+                print(f"[ENVIADO] Pacote {pacotes_enviados}: {len(chunk)} bytes | Total enviado: {bytes_enviados} bytes")
+        
+        # Termina a transmissão de dados para este arquivo
+        client_socket.send(b"END")
+
+        # Receber o tempo do servidor
+        server_time = client_socket.recv(1024).decode()
+        end_time = float(server_time)
+        total_time = end_time - start_time
+
+        print(f"[ESTATISTICA] Arquivo {file_name} de tamanho {file_size} bytes foi enviado com sucesso.")
+        print(f"[ESTATISTICA] Pacotes enviados: {pacotes_enviados}")
+        print(f"[ESTATISTICA] Tempo total da transferência: {total_time:.6f} segundos")
+
     client_socket.close()
-    
-    end_time = float(server_time)
-    total_time = end_time - start_time
-    print(f"[ESTATISTICA] Arquivo {file_name} de tamanho {file_size} bytes foi enviado com sucesso.")
-    print(f"[ESTATISTICA] Pacotes enviados: {pacotes_enviados}")
-    print(f"[ESTATISTICA] Tempo total da transferência: {total_time:.6f} segundos")
-    
+    print("[INFO] Conexão com o servidor encerrada.")
+
 if __name__ == "__main__":
-    start_tcp_client('192.168.1.2', 5000)
+    start_tcp_client('192.168.1.2', 5001)
